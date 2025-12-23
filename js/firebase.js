8dc8ae61bad16bf9a65abd040e215090394a653a
jonatanoficial-bit <jonatanoficial@gmail.com>
2025-12-22 21:46:50 -0300

    js/firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,24 @@
+// js/firebase.js
+// Firebase modular SDK (CDN) - v12.7.0
+
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-storage.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSOuLs1PVG4eGn0NSNZxksJP8IqIdURrE",
+  authDomain: "imvapp-aef54.firebaseapp.com",
+  projectId: "imvapp-aef54",
+  storageBucket: "imvapp-aef54.firebasestorage.app",
+  messagingSenderId: "439661516200",
+  appId: "1:439661516200:web:2d3ede20edbb9aa6d6f99d",
+  measurementId: "G-2LEK7QDZ48"
+};
+
+// Garante que não inicialize duplicado
+export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
