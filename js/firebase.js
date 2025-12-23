3de7b9b975389d57f9ad000ec3a5d512d355d735
jonatanoficial-bit <jonatanoficial@gmail.com>
2025-12-23 19:49:40 -0300

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,13 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
+// ✅ Config REAL do seu projeto (imvapp-aef54)
+// (Foi a que você mostrou no print do Firebase)
 const firebaseConfig = {
   apiKey: "AIzaSyCSOuLs1PVG4eGn0NSNZxksJP8IqIdURrE",
   authDomain: "imvapp-aef54.firebaseapp.com",
   projectId: "imvapp-aef54",
   storageBucket: "imvapp-aef54.firebasestorage.app",
   messagingSenderId: "439661516200",
-  appId: "1:439661516200:web:2d3ede20edbb9aa6d6f99d"
+  appId: "1:439661516200:web:2d3ede20edbb9aa6d6f99d",
+  measurementId: "G-2LEK7QDZ48"
 };
 
 export const app = initializeApp(firebaseConfig);
